342a71e12058064527082809774a5f15d9b5ad84
RayaneAll <9975@holbertonstudents.com>
2025-12-08 18:29:16 +0100

    Correction of 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F': *' '/^(Registrant|Admin|Tech) (Name|Organization|Street|City|State\/Province|Postal Code|Country|Phone|Phone Ext|Fax|Fax Ext|Email)/{k=$1;v=$2;if(k~/(Phone|Fax) Ext$/){k=k ":"}if(k~/Street$/){v=v " "}gsub(" ","$",k);gsub(" ","$",v);print k "," v}' > $1.csv
+whois "$1" | awk 'BEGIN { FS=": "; OFS="," } /^(Registrant|Admin|Tech)/ {print $1, $2}' > "$1.csv"
